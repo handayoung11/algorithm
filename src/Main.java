9dfcdc5c318d1750937a173b069df046a7ef9d36
JinYeongKim <handayoung11@gmail.com>
2021-09-16 17:31:16 +0900

    백준 1065 - 한수 풀이 주석추가 및 코드 format

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
+
 import static java.lang.Integer.parseInt;
 
 public class Main {
@@ -12,25 +13,28 @@ public class Main {
     }
 
     public static int findNumber(int num) {
-        int count = 0;
-       for(int i = 1; i<= num; i++) {
-           int[] intArr = Arrays.stream((i + "").split("")).mapToInt(s -> parseInt(s)).toArray();
-           if(intArr.length <= 2) {
-               count++;
-               continue;
-           }
+        // 99이하의 숫자는 반드시 한수이므로 num 중 99이하인 수를 기본 count로 설정
+        int count = num >= 99 ? 99 : num;
 
-           int dif = intArr[1] - intArr[0];
-           boolean isNumber = true;
-           for (int j = 2; j < intArr.length; j++)
-               if(intArr[j] != dif + intArr[j - 1]) {
-                   isNumber = false;
-                   break;
-               }
+        // 99까지는 체크완료했으므로 100부터 시작
+        for (int i = 100; i <= num; i++) {
+            // 입력받은 숫자를 자리별로 분해하여 배열로 보관
+            int[] intArr = Arrays.stream((i + "").split("")).mapToInt(s -> parseInt(s)).toArray();
+            // 공차 구하기
+            int dif = intArr[1] - intArr[0];
 
-           if(isNumber) count++;
-       }
+            boolean isNumber = true;
+            for (int j = 2; j < intArr.length; j++)
+                // 공차 + 이전수 = 현재수가 성립하는지 검사
+                if (intArr[j] != dif + intArr[j - 1]) {
+                    isNumber = false;
+                    break;
+                }
 
-       return count;
+            // 모든 자릿수에서 공차 + 이전수 = 현재수가 성립하면 count++
+            if (isNumber) count++;
+        }
+
+        return count;
     }
 }
