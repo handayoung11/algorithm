9bfa592cb0885f8afc7025c57284f06b2470fad3
JinYeongKim <handayoung11@gmail.com>
2021-10-26 01:02:48 +0900

    백준 1018 - 체스판 다시 칠하기 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,52 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    // 백준 2231 분해합 - 완전탐색
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int nunmber = scanner.nextInt();
-        System.out.println(findGenerator(nunmber));
+    static boolean colors[][];
+
+    // 백준 1018 - 체스판 다시 칠하기
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(bufferedReader.readLine());
+        int row = strToInt(tokenizer.nextToken()), column = strToInt(tokenizer.nextToken());
+
+        // true = B, false = W
+        colors = new boolean[row][column];
+
+        for (int i = 0; i < row; i++) {
+            String str = bufferedReader.readLine();
+            for (int j = 0; j < column; j++) colors[i][j] = str.charAt(j) == 'B';
+        }
+
+        // 잘못된 블럭개수의 최대값인 32 초기값으로 지정
+        int count = 32;
+        for (int i = 0; 8 <= row - i; i++) {
+            for (int j = 0; 8 <= column - j; j++) {
+                count = Math.min(count, getUnmatchedBlock(i, j));
+            }
+            if(count == 0) break;
+        }
+
+        System.out.println(count);
     }
 
-    public static int findGenerator(int number) {
-        // 1에서부터 분해합을 찾아나간다.
-        for(int i = 1; i < number; i++) {
-            // 분해합 구하기
-            int digitSum = i, n = i;
-            while(n > 0) {
-                digitSum += n % 10; // 맨 끝자리를 더한다
-                n /= 10; // n을 10으로 나눠 1의 자리를 없앤다
-            }
-            if(digitSum == number) return i;
+    private static int strToInt(String str) {
+        return Integer.parseInt(str);
+    }
+
+    private static int getUnmatchedBlock(int rowStart, int colStart) {
+        boolean start = true;
+        int wrongBlock = 0;
+        for(int i = rowStart; i < rowStart + 8; i++) {
+            // 색깔 전환해가며 다음 색깔과 비교 반복
+            for(int j = colStart; j < colStart + 8; j++) if(colors[i][j] != (start = !start)) wrongBlock++;
+            // 행 전환 시 색깔 전환
+            start = !start;
         }
-        return 0;
+
+        // 64 - W 시작 기준의 잘못된 블럭 개수 = B 시작 기준의 잘못된 블럭 개수
+        // 둘 중 더 작은 수 반환
+        return Math.min(wrongBlock, 64 - wrongBlock);
     }
 }
