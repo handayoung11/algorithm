521d1167b1461d9582ada3183396277fba687e90
JinYeongKim <handayoung11@gmail.com>
2021-11-04 01:38:43 +0900

    백준 1062 - 가르침 문제 풀이

--- src/Main.java
+++ src/Main.java
@@ -1,90 +1,59 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-    static int MAX = 0, N;
-    // 상하좌우
-    static int xyMove[][] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    static boolean calculated[][];
+    static int MAX = 0;
+    static int[] words;
+    static int N, K;
 
-    // 백준 12100 - 2048 (Easy)
+    // 백준 1062 - 가르침
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        N = strToInt(bufferedReader.readLine());
-        int data[][] = new int[N][N];
-        calculated = new boolean[N][N];
+        StringTokenizer token = new StringTokenizer(bufferedReader.readLine());
+        N = strToInt(token.nextToken());
+        K = strToInt(token.nextToken());
+        words = new int[N];
+        int alphabet = 0, whole = 0;
+        char essential[] = {'a', 'n', 't', 'i', 'c'};
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer token = new StringTokenizer(bufferedReader.readLine());
-            for (int j = 0; j < N; j++) data[i][j] = strToInt(token.nextToken());
+        for (char c : essential) alphabet |= 1 << (c - 'a');
+
+        for(int i = 0; i < N; i++) {
+            String word = bufferedReader.readLine();
+            words[i] = alphabet;
+            for(int j = 4; j < word.length() - 4; j++) words[i] |= 1 << (word.charAt(j) - 'a');
+            whole |= words[i];
         }
 
-        if(N == 1) {
-            System.out.println(data[0][0]);
+        if(K < 5) {
+            System.out.println("0");
+            return;
+        } else if(Integer.bitCount(whole) <= K) {
+            System.out.println(N + "");
             return;
         }
-        dfs(data, 0);
+
+        dfs(alphabet, -1);
         System.out.println(MAX + "");
     }
 
-    // 이동이 일어난 후의 결과값을 얻어오는 dfs
-    private static void dfs(int[][] data, int len) {
-        if (len == 5) return;
-
-        int source[][] = new int[N][N];
-        for (int i = 0; i < N; i++) source[i] = data[i].clone();
-        for (int i = 0; i < 4; i++) {
-            int x = 0, y = 0;
-            if (xyMove[i][0] == -1 || xyMove[i][1] == -1) {
-                x = xyMove[i][0] * -1;
-                y = xyMove[i][1] * -1;
-            } else {
-                if(xyMove[i][0] == 0) y = N - 2;
-                else x = N - 2;
+    private static void dfs(int learnedWords, int last) {
+        if(Integer.bitCount(learnedWords) == K) {
+            int cnt = 0, check;
+            for(int i = 0; i < N; i++) {
+                check = learnedWords | words[i];
+                if(check == learnedWords) cnt++;
             }
-            calculated = new boolean[N][N];
-            blockMove(y, x, i, data, false);
-            dfs(data, len + 1);
-            for (int j = 0; j < N; j++) data[j] = source[j].clone();
-        }
-    }
-
-    //입력받은 좌표를 기점으로 moveIdx에 따라 한 칸 움직이는 함수
-    private static void blockMove(int y, int x, int moveIdx, int result[][], boolean fromZero) {
-        int nextY = y + xyMove[moveIdx][1];
-        int nextX = x + xyMove[moveIdx][0];
-        if (nextY < 0 || nextY >= N || nextX < 0 || nextX >= N || calculated[y][x]) return;
-
-        // 블록 밀기
-        int v1 = result[y][x], v2 = result[nextY][nextX];
-        if (v2 == 0) {
-            result[y][x] = 0;
-            result[nextY][nextX] += v1;
-            if(v1 != 0) blockMove(nextY, nextX, moveIdx, result, true);
-        } else if (v1 == v2) {
-            result[y][x] = 0;
-            result[nextY][nextX] += v1;
-            calculated[y][x] = true;
+            MAX = Math.max(cnt, MAX);
+            return;
         }
 
-        MAX = Math.max(MAX, result[x][y]);
-        if(fromZero) return; //0에서 온 경우 다음 인덱스로 갈 필요 X
-
-        if (xyMove[moveIdx][0] == 0) {
-            x++;
-            if(x >= N) {
-                x = 0;
-                y += xyMove[moveIdx][1] * -1;
-            }
-        } else {
-            y++;
-            if(y >= N) {
-                y = 0;
-                x += xyMove[moveIdx][0] * -1;
-            }
+        for(int i = last + 1; i < 26; i++) {
+            if ((learnedWords & (1 << i)) != 0) continue;
+            dfs(learnedWords | 1 << i, i);
         }
-        if (y < 0 || y >= N || x < 0 || x >= N) return;
-        blockMove(y, x, moveIdx, result, false);
     }
 
     private static int strToInt(String str) {
