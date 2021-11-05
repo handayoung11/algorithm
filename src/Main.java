a4cc0c83dc574cae36ebfa2c3d3d397c6c4989d1
JinYeongKim <handayoung11@gmail.com>
2021-11-05 18:13:00 +0900

    백준 1057 - 토너먼트 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -4,56 +4,42 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int MAX = 0;
-    static int[] words;
-    static int N, K;
+    static int cnt, num[], round = 1;
+    static StringBuilder matchList = new StringBuilder();
 
-    // 백준 1062 - 가르침
+    // 백준 1057 - 토너먼트
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer token = new StringTokenizer(bufferedReader.readLine());
-        N = strToInt(token.nextToken());
-        K = strToInt(token.nextToken());
-        words = new int[N];
-        int alphabet = 0, whole = 0;
-        char essential[] = {'a', 'n', 't', 'i', 'c'};
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        cnt = strToInt(token.nextToken());
+        num = new int[]{strToInt(token.nextToken()), strToInt(token.nextToken())};
 
-        for (char c : essential) alphabet |= 1 << (c - 'a');
+        while(matchList.length() < cnt) matchList.insert(0, 0);
+        matchList.replace(num[0] - 1, num[0], "1");
+        matchList.replace(num[1] - 1, num[1], "1");
+        match();
 
-        for(int i = 0; i < N; i++) {
-            String word = bufferedReader.readLine();
-            words[i] = alphabet;
-            for(int j = 4; j < word.length() - 4; j++) words[i] |= 1 << (word.charAt(j) - 'a');
-            whole |= words[i];
-        }
-
-        if(K < 5) {
-            System.out.println("0");
-            return;
-        } else if(Integer.bitCount(whole) <= K) {
-            System.out.println(N + "");
-            return;
-        }
-
-        dfs(alphabet, -1);
-        System.out.println(MAX + "");
+        System.out.println(round);
     }
 
-    private static void dfs(int learnedWords, int last) {
-        if(Integer.bitCount(learnedWords) == K) {
-            int cnt = 0, check;
-            for(int i = 0; i < N; i++) {
-                check = learnedWords | words[i];
-                if(check == learnedWords) cnt++;
-            }
-            MAX = Math.max(cnt, MAX);
+    static void match() {
+        StringBuilder nextMatchList = new StringBuilder();
+        for(int i = 0; i < cnt - 1; i+=2) {
+            int p = ((matchList.charAt(i) - '0') + (matchList.charAt(i + 1) - '0'));
+            if(p == 2) return;
+            nextMatchList.append(p);
+        }
+        if(cnt != nextMatchList.length()) nextMatchList.append(matchList.charAt(cnt - 1));
+        matchList = nextMatchList;
+
+        if(cnt <= 1) {
+            round = -1;
             return;
         }
 
-        for(int i = last + 1; i < 26; i++) {
-            if ((learnedWords & (1 << i)) != 0) continue;
-            dfs(learnedWords | 1 << i, i);
-        }
+        cnt = (cnt + 1) / 2;
+        round++;
+        match();
     }
 
     private static int strToInt(String str) {
