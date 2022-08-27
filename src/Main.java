de1173fc5f95e7de05138f528d2d467a26807015
JinYeongKim <handayoung11@gmail.com>
2022-08-27 22:40:05 +0900

    백준 9663 - N-Queen 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-    static int totalX, totalY, target, vx, vy; //X값 총합,Y값 총합
-    static int coor[][]; //좌표 배열
-    static double min;
+    static int board[], N, ans;
 
-    // 백준 1007 - 벡터 매칭
+    // 백준 9663 - N-Queen
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            int n = Integer.parseInt(br.readLine());
-            target = n / 2;
-            //coor 구조 = [[x: 0, y: 1], [0, 2]]
-            coor = new int[n][2];
-            totalX = 0;
-            totalY = 0;
-            min = Double.POSITIVE_INFINITY;
-
-            for (int j = 0; j < n; j++) {
-                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-                int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
-                coor[j] = new int[]{x + x, y + y};
-                totalX += x;
-                totalY += y;
-            }
-
-            for (int j = 0; j < coor.length; j++) {
-                vx = totalX;
-                vy = totalY;
-                if (coor.length - j >= target) {
-                    getMinVec(j, 1);
-                }
-            }
-            System.out.println(Math.sqrt(min));
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            board = new int[N];
+            putQueen(i, 0);
         }
+
+        System.out.println(ans);
     }
 
-    private static void getMinVec(int idx, int depth) {
-        vx -= coor[idx][0];
-        vy -= coor[idx][1];
-
-        if (depth >= target) {
-            min = Math.min((long)vx * vx + (long)vy * vy, min);
+    private static void putQueen(int x, int y) {
+        if (y == N - 1) {
+            ans++;
+            return;
         }
-        else {
-            ++depth;
-            for (int i = idx + 1; i < coor.length; i++) {
-                if (coor.length - i >= target - depth + 1) {
-                    getMinVec(i, depth);
+
+        board[y] = x;
+
+        int nextY = y + 1;
+        outer:
+        for (int j = 0; j < N; j++) {
+            for (int i = 0; i < nextY; i++) {
+                if (j == board[i] || Math.abs(board[i] - j) == nextY - i) {
+                    continue outer;
                 }
             }
+
+            putQueen(j, y + 1);
         }
-        vx += coor[idx][0];
-        vy += coor[idx][1];
     }
 }
