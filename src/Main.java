486628786f29eb292663193dc0ba60708e120bb9
JinYeongKim <handayoung11@gmail.com>
2023-03-20 16:02:47 +0900

    백준 1047 - 울타리 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,112 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
-// 백준 1038 - 감소하는 수
+import static java.lang.Integer.parseInt;
+
+// 백준 1047 - 울타리
 public class Main {
+
+    static Tree[] trees;
+    static int[] x, y;
+    static int ans;
+    static int N;
+    static Rectangle r;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        N = parseInt(br.readLine());
+        trees = new Tree[N];
+        x = new int[N];
+        y = new int[N];
+        ans = N - 1;
 
-        if (N <= 10) {
-            System.out.println(N);
-            return;
-        } else if (N >= 1023) {
-            System.out.println(-1);
-            return;
+        for (int i = 0; i < N; i++) {
+            String s[] = br.readLine().split(" ");
+            trees[i] = new Tree(parseInt(s[0]), parseInt(s[1]), parseInt(s[2]));
+            x[i] = trees[i].x;
+            y[i] = trees[i].y;
         }
+        Arrays.sort(trees, Comparator.comparingInt((Tree t) -> t.len).reversed());
+        Arrays.sort(x);
+        Arrays.sort(y);
 
-        int target = 11;
-        long num = 20;
-        while(true) {
-            String s = num + "";
-            boolean isDecreased = true;
-            int len = s.length();
-
-            for (int i = 0; i < len - 1; i++) {
-                if (s.charAt(i) <= s.charAt(i + 1)) {
-                    isDecreased = false;
-                    int digit = (int) Math.pow(10, len - 1 - i);
-                    num = (num / digit * digit) + digit;
-                    break;
+        //x좌표와 y좌표를 조합해서 모든 경우의 사각형을 만든다
+        int minX, minY, maxX, maxY;
+        for (int a = 0; a < N - 1; a++) {
+            minX = x[a];
+            for (int b = a + 1; b < N; b++) {
+                maxX = x[b];
+                for (int c = 0; c < N - 1; c++) {
+                    minY = y[c];
+                    for (int d = c + 1; d < N; d++) {
+                        maxY = y[d];
+                        r = new Rectangle(minX, minY, maxX - minX, maxY - minY);
+                        cutDown();
+                    }
                 }
             }
+        }
 
-            if (isDecreased) {
-                if (target == N) {
-                    System.out.println(num);
+        System.out.println(ans);
+    }
+
+    static void cutDown() {
+        int cutted = 0;
+        int len = 0;
+
+        Queue<Integer> contained = new LinkedList<>();
+        for (Tree t : trees) {
+            // 사각형 안에 포함되는 경우
+            if (contained(t)) {
+                contained.add(t.len);
+            } else {
+                // 포함되지 않는 경우, 나무를 자르고 길이를 추가한다
+                cutted++;
+                if (cutted == ans) {
                     return;
                 }
-                num++;
-                target++;
+                len += t.len;
             }
         }
+
+        // 바깥쪽의 나무를 잘라서 둘레를 충족시킨 경우
+        int perimeter = (r.width + r.height) * 2;
+        if (perimeter <= len) {
+            ans = cutted;
+            return;
+        }
+
+        // 그렇지 못한 경우 안쪽의 나무들도 잘라서 둘레를 충족시킨다
+        Integer l;
+        while ((l = contained.poll()) != null) {
+            cutted++;
+            if (cutted == ans) {
+                return;
+            }
+            len += l;
+            if (perimeter <= len) {
+                ans = cutted;
+                return;
+            }
+        }
+    }
+
+    static boolean contained(Tree t) {
+        return r.x <= t.x && r.x + r.width >= t.x && r.y <= t.y && r.y + r.height >= t.y;
+    }
+}
+
+class Tree {
+    int x;
+    int y;
+    int len;
+
+    public Tree(int x, int y, int len) {
+        this.x = x;
+        this.y = y;
+        this.len = len;
     }
 }
