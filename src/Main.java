0bdeba1eb5a24748cffb5c557a72c1467bfcbd4e
JinYeongKim <handayoung11@gmail.com>
2025-03-26 17:22:34 +0900

    백준 9012 - 괄호 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 백준 9093 - 단어 뒤집기
+// 백준 9012 - 괄호
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char st[] = new char[20];
-        int top = -1;
         StringBuilder b = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
-            StringBuilder w = new StringBuilder(br.readLine()).append(' ');
+            StringBuilder w = new StringBuilder(br.readLine());
+            int cnt = 0;
 
             for (int j = 0; j < w.length(); j++) {
-                if (w.charAt(j) == ' ') {
-                    while(top != -1)
-                        b.append(st[top--]);
-                    b.append(' ');
-                } else st[++top] = w.charAt(j);
+                if (w.charAt(j) == ')') cnt--;
+                else cnt++;
+
+                if (cnt < 0) {
+                    b.append("NO\n");
+                    break;
+                } else if(j == w.length() - 1) {
+                    b.append(cnt == 0 ? "YES\n" : "NO\n");
+                }
             }
-            b.append('\n');
         }
-        System.out.print(b);
+        System.out.println(b);
     }
 }
