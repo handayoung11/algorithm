da5dbdc2a9713155e803a42f9bc16d4e60f13033
JinYeongKim <handayoung11@gmail.com>
2025-03-31 17:35:03 +0900

    백준 1406 - 에디터 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,52 @@
 import java.io.*;
+import java.util.LinkedList;
 
-// 백준 1874 - 스택 수열
+// 백준 1406 - 에디터
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        String text = br.readLine();
+        int N = Integer.parseInt(br.readLine());
+        char[] left = new char[600000], right = new char[600000];
+        int lTop = text.length() - 1, rTop = -1;
 
-        int n = Integer.parseInt(br.readLine());
-        int st[] = new int[100000], top = -1, now, c = 1;
+        for (int i = 0; i < text.length(); i++)
+            left[i] = text.charAt(i);
 
-        for (int i = 0; i < n; i++) {
-            now = Integer.parseInt(br.readLine());
-
-            while (top == -1 || st[top] < now) {
-                st[++top] = c++;
-                sb.append("+\n");
-            }
-
-            if (st[top] == now) {
-                top--;
-                sb.append("-\n");
-            } else {
-                System.out.print("NO");
-                return;
+        for (int i = 0; i < N; i++) {
+            String command = br.readLine();
+            switch (command) {
+                case "L": {
+                    if (lTop != -1) {
+                        right[++rTop] = left[lTop--];
+                    }
+                    break;
+                }
+                case "D": {
+                    if (rTop != -1) {
+                        left[++lTop] = right[rTop--];
+                    }
+                    break;
+                }
+                case "B": {
+                    if (lTop != -1) {
+                        lTop--;
+                    }
+                    break;
+                }
+                default: {
+                    char p = command.charAt(2);
+                    left[++lTop] = p;
+                }
             }
         }
 
-        System.out.println(sb);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < lTop + 1; i++)
+            bw.write(left[i]);
+        for (int i = rTop; i >= 0; i--)
+            bw.write(right[i]);
+        bw.flush();
     }
 }
