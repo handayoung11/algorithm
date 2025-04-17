4c2621573b04a958e621395a8bee67bc0c37dc5e
JinYeongKim <handayoung11@gmail.com>
2025-04-17 23:10:38 +0900

    백준 10799 - 쇠막대기 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 17413 - 단어 뒤집기 2
+// 10799 - 쇠막대기
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine() + " ";
+        String s = br.readLine();
+        
+        int cnt = 0, opened = 0;
 
-        int idx = 0;
-        int front = 0;
-        StringBuilder sb = new StringBuilder();
-        while (idx < s.length()) {
-            if(s.charAt(idx) == '<' || s.charAt(idx) == ' ') {
-                for (int i = idx - 1; i >= front; i--)
-                    sb.append(s.charAt(i));
-
-                if (s.charAt(idx) == '<') {
-                    while (s.charAt(idx) != '>') {
-                        sb.append(s.charAt(idx++));
-                    }
-                }
-
-                sb.append(s.charAt(idx++));
-                front = idx;
-            } else idx++;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                if (s.charAt(i + 1) == ')') { //isLaser
+                    i++;
+                    cnt += opened;
+                } else opened++;
+            } else {
+                opened--;
+                cnt++;
+            }
         }
-
-        System.out.println(sb);
+        System.out.println(cnt);
     }
 }
