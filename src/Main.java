b9387db3b7601dd44efcedefad2eb5b2175fc49b
JinYeongKim <handayoung11@gmail.com>
2025-04-17 22:30:55 +0900

    백준 17413 - 단어 뒤집기 2 문제풀이

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
 
-// 10866 - 덱(배열로 풀기)
+// 17413 - 단어 뒤집기 2
 public class Main {
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine() + " ";
+
+        int idx = 0;
+        int front = 0;
         StringBuilder sb = new StringBuilder();
+        while (idx < s.length()) {
+            if(s.charAt(idx) == '<' || s.charAt(idx) == ' ') {
+                for (int i = idx - 1; i >= front; i--)
+                    sb.append(s.charAt(i));
 
-        int N = Integer.parseInt(br.readLine());
-        int[] deque = new int[N];
-        int front = 0, back = 0, size = 0;
-
-        for (int i = 0; i < N; i++) {
-            String command = br.readLine();
-
-            switch (command) {
-                case "front":
-                case "back":
-                case "pop_front":
-                case "pop_back": {
-                    if (size == 0) sb.append("-1\n");
-                    else {
-                        switch (command) {
-                            case "pop_front": size--;
-                            case "front":
-                                sb.append(deque[front]).append("\n");
-                                if (command.equals("pop_front"))
-                                    if (++front == N) front = 0;
-                                break;
-                            case "pop_back": size--;
-                            case "back":
-                                int idx = back - 1;
-                                if (idx == -1) idx = N - 1;
-                                sb.append(deque[idx]).append("\n");
-                                if (command.equals("pop_back"))
-                                    back = idx;
-                        }
-                    }
-                    break;
-                } case "size": {
-                    sb.append(size).append("\n");
-                    break;
-                } case "empty": {
-                    if (size == 0) sb.append("1\n");
-                    else sb.append("0\n");
-                    break;
-                } default: {
-                    size++;
-                    String[] comX = command.split(" ");
-                    int X = Integer.parseInt(comX[1]);
-                    if (comX[0].equals("push_front")) {
-                        if (--front == -1)
-                            front = N - 1;
-                        deque[front] = X;
-                    } else {
-                        deque[back] = X;
-                        if (++back == N) back = 0;
+                if (s.charAt(idx) == '<') {
+                    while (s.charAt(idx) != '>') {
+                        sb.append(s.charAt(idx++));
                     }
                 }
-            }
+
+                sb.append(s.charAt(idx++));
+                front = idx;
+            } else idx++;
         }
-        System.out.print(sb);
+
+        System.out.println(sb);
     }
 }
